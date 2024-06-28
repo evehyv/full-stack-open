ad72c4ea1f4ab0e54d5df138abdc9a2c79490afb
evehyv <evehyv@gmail.com>
2024-06-28 19:18:04 +0300

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,8 +4,8 @@ const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Display = ({ text, value }) => (
-  <div>{text} {value}</div>
+const Display = ({ value }) => (
+  <div>{value}</div>
 )
 
 const App = () => {
@@ -13,17 +13,54 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allFeedback, setAllFeedback] = useState([])
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedAllFeedback = allFeedback.concat(1)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    updateStatistics(updatedAllFeedback)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedAllFeedback = allFeedback.concat(0)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    updateStatistics(updatedAllFeedback)
+  }
+
+  const handleBadClick = () => {
+    const updatedAllFeedback = allFeedback.concat(-1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    updateStatistics(updatedAllFeedback)
+  }
+
+  const updateStatistics = (updatedAllFeedback) => {
+    const sumOfAll = updatedAllFeedback.reduce((sum, value) => sum + value, 0)
+    const updatedAverage = sumOfAll / updatedAllFeedback.length
+    const positiveFeedbackCount = updatedAllFeedback.filter(value => value === 1).length
+    const updatedPositive = (positiveFeedbackCount / updatedAllFeedback.length) * 100
+    setAllFeedback(updatedAllFeedback)
+    setAverage(updatedAverage)
+    setPositive(updatedPositive)
+  }
 
   return (
     <>
       <h1>Give feedback</h1>
-      <Button text='Good' handleClick={() => setGood(good + 1)} />
-      <Button text='Neutral' handleClick={() => setNeutral(neutral + 1)} />
-      <Button text='Bad' handleClick={() => setBad(bad + 1)} />
+      <Button text='Good' handleClick={handleGoodClick} />
+      <Button text='Neutral' handleClick={handleNeutralClick} />
+      <Button text='Bad' handleClick={handleBadClick} />
       <h1>Statistics</h1>
-      <Display text='Good' value={good} />
-      <Display text='Neutral' value={neutral} />
-      <Display text='Bad' value={bad} />
+      <Display value={`Good ${good}`} />
+      <Display value={`Neutral ${neutral}`} />
+      <Display value={`Bad ${bad}`} />
+      <Display value={`All ${allFeedback.length}`} />
+      <Display value={`Average ${average}`} />
+      <Display value={`Positive ${positive} %`} />
     </>
   )
 }
