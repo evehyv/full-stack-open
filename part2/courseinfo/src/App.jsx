4a0a7cfae60bfb83d506c03b5a2879a970b1c809
evehyv <evehyv@gmail.com>
2024-07-01 16:11:16 +0300

    2.5: Separate module step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,23 +1,4 @@
-const Header = ({ courseName }) => <h2>{courseName}</h2>
-
-const Total = ({ sum }) => <p><b>Total of {sum} exercises</b></p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}    
-  </>
-
-const Course = ({ course }) => 
-  <>
-    <Header courseName={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={course.parts.reduce((accumulator, currentPart) => accumulator + currentPart.exercises, 0)} />
-  </>
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
