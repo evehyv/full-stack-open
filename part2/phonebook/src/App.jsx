568ded7c99a94300f4bcf64ee69aa3186650eb76
evehyv <evehyv@gmail.com>
2024-07-02 10:47:21 +0300

    2.7: The Phonebook Step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,9 +12,15 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = { name: newName }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    const personExists = persons.some(person => person.name === newName)
+
+    if (personExists) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const personObject = { name: newName }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
   }
 
   return (
