6f5abf34915071d7680ea37c72c3e3ce7a41929a
evehyv <evehyv@gmail.com>
2024-07-16 16:25:07 +0300

    3.5: Phonebook backend step 5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -44,6 +46,26 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+
+const generateRandomId = () => {
+  const randomId = Math.floor(Math.random() * 10000);
+  return String(randomId)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateRandomId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 app.get('/info', (request, response) => {
