6cac9bc6e6a4988318f7fd216e17b778e2f8192c
evehyv <evehyv@gmail.com>
2024-07-18 12:46:11 +0300

    3.8*: Phonebook backend step 8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('person', (req) => JSON.stringify(req.body) )
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = [
     { 
